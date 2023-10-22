824dede99e78127c3e915cc80e6441a9578c862a
kolike <kolik_2604@mail.ru>
2023-10-22 20:33:15 +0300

    refactor welcome banner

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,18 +1,18 @@
-import "./App.css";
-import WelcomeBanner from "../banner/welcomeBanner";
-import PhoneNumberPage from "../phoneNumberPage/phoneNumberPage";
-import FinnalyPage from "../finallyPage/finnalyPage";
-import VideoComponent from "../videoComponent/videoComponent";
+import './App.css';
+import PhoneNumberPage from '../phoneNumberPage/phoneNumberPage';
+import FinnalyPage from '../finallyPage/finnalyPage';
+import VideoComponent from '../videoComponent/videoComponent';
+import HomePage from '../homePage/homePage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-        <VideoComponent/>
+      <VideoComponent />
       <Routes>
-        <Route path="/" element={<WelcomeBanner />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/phone" element={<PhoneNumberPage />} />
-        <Route path="/finnaly" element={<FinnalyPage/>} />
+        <Route path="/finnaly" element={<FinnalyPage />} />
       </Routes>
     </Router>
   );
---
+++ src/components/homePage/homePage.js
@@ -0,0 +1,17 @@
+import WelcomeBanner from '../banner/welcomeBanner';
+import { useEffect, useState } from 'react';
+
+const HomePage = () => {
+  const [isBannerVisible, setIsBannerVisible] = useState(false);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setIsBannerVisible(true);
+    }, 5000);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, []);
+  return <>{isBannerVisible && <WelcomeBanner />}</>;
+};
+export default HomePage;
